9977bfd7191b5dceefa9a2812f06ca1462c4d330
Limonchik666 <156921324+Limonchik666@users.noreply.github.com>
2024-07-01 20:32:07 +0300

    Add files via upload

---
+++ New Rock, Paper, Scissors/New Rock, Paper, Scissors/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace New_Rock__Paper__Scissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Scissors = new System.Windows.Forms.Button();
+            this.Paper = new System.Windows.Forms.Button();
+            this.Rock = new System.Windows.Forms.Button();
+            this.eRock = new System.Windows.Forms.PictureBox();
+            this.ePaper = new System.Windows.Forms.PictureBox();
+            this.eScissors = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.eRock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ePaper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eScissors)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Scissors
+            // 
+            this.Scissors.Image = global::New_Rock__Paper__Scissors.Properties.Resources.Scissors;
+            this.Scissors.Location = new System.Drawing.Point(527, 300);
+            this.Scissors.Name = "Scissors";
+            this.Scissors.Size = new System.Drawing.Size(130, 130);
+            this.Scissors.TabIndex = 2;
+            this.Scissors.UseVisualStyleBackColor = true;
+            this.Scissors.Click += new System.EventHandler(this.Scissors_Click);
+            // 
+            // Paper
+            // 
+            this.Paper.Image = global::New_Rock__Paper__Scissors.Properties.Resources.Paper;
+            this.Paper.Location = new System.Drawing.Point(300, 300);
+            this.Paper.Name = "Paper";
+            this.Paper.Size = new System.Drawing.Size(130, 130);
+            this.Paper.TabIndex = 1;
+            this.Paper.UseVisualStyleBackColor = true;
+            this.Paper.Click += new System.EventHandler(this.Paper_Click);
+            // 
+            // Rock
+            // 
+            this.Rock.Image = global::New_Rock__Paper__Scissors.Properties.Resources.Rock1;
+            this.Rock.Location = new System.Drawing.Point(74, 300);
+            this.Rock.Name = "Rock";
+            this.Rock.Size = new System.Drawing.Size(130, 130);
+            this.Rock.TabIndex = 0;
+            this.Rock.UseVisualStyleBackColor = true;
+            this.Rock.Click += new System.EventHandler(this.Rock_Click);
+            // 
+            // eRock
+            // 
+            this.eRock.Image = global::New_Rock__Paper__Scissors.Properties.Resources.Rock;
+            this.eRock.Location = new System.Drawing.Point(300, -130);
+            this.eRock.Name = "eRock";
+            this.eRock.Size = new System.Drawing.Size(130, 130);
+            this.eRock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.eRock.TabIndex = 3;
+            this.eRock.TabStop = false;
+            // 
+            // ePaper
+            // 
+            this.ePaper.Image = global::New_Rock__Paper__Scissors.Properties.Resources.Paper;
+            this.ePaper.Location = new System.Drawing.Point(300, -130);
+            this.ePaper.Name = "ePaper";
+            this.ePaper.Size = new System.Drawing.Size(130, 130);
+            this.ePaper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ePaper.TabIndex = 4;
+            this.ePaper.TabStop = false;
+            // 
+            // eScissors
+            // 
+            this.eScissors.Image = global::New_Rock__Paper__Scissors.Properties.Resources.Scissors;
+            this.eScissors.Location = new System.Drawing.Point(300, -130);
+            this.eScissors.Name = "eScissors";
+            this.eScissors.Size = new System.Drawing.Size(130, 130);
+            this.eScissors.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.eScissors.TabIndex = 5;
+            this.eScissors.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.eScissors);
+            this.Controls.Add(this.ePaper);
+            this.Controls.Add(this.eRock);
+            this.Controls.Add(this.Scissors);
+            this.Controls.Add(this.Paper);
+            this.Controls.Add(this.Rock);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.eRock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ePaper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eScissors)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Rock;
+        private System.Windows.Forms.Button Paper;
+        private System.Windows.Forms.Button Scissors;
+        private System.Windows.Forms.PictureBox eRock;
+        private System.Windows.Forms.PictureBox ePaper;
+        private System.Windows.Forms.PictureBox eScissors;
+    }
+}
+
---
+++ New Rock, Paper, Scissors/New Rock, Paper, Scissors/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace New_Rock__Paper__Scissors
+{
+    public partial class Form1 : Form
+    {
+        public int enemy, player = 4;
+        public string winner, tPlayer, tEnemy;
+        public Label WhoWinner;
+        public Random random = new Random();
+
+        private void Rock_Click(object sender, EventArgs e)
+        {
+            this.player = 0;
+            Logic();
+        }
+
+        private void Paper_Click(object sender, EventArgs e)
+        {
+            this.player = 1;
+            Logic();
+        }
+
+        private void Scissors_Click(object sender, EventArgs e)
+        {
+            this.player = 2;
+            Logic();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            WeaponEnemy();
+            Logic();
+
+            WhoWinner = new Label();
+            WhoWinner.Text = $"Выберите оружие";
+            WhoWinner.Location = new Point(300, 145);
+            WhoWinner.Width = 200;
+            WhoWinner.Height = 80;
+            this.Controls.Add(WhoWinner);
+        }
+
+        public void Logic()
+        {
+            WeaponEnemy();
+
+            if (player == 0 && enemy == 0)
+            {
+                winner = "дружба";
+                eRock.Location = new Point(300, 10);
+                ePaper.Location = new Point(300, -130);
+                eScissors.Location = new Point(300, -130);
+            }
+            else if (player == 0 && enemy == 1)
+            {
+                winner = "Игрок";
+                eRock.Location = new Point(300, -130);
+                ePaper.Location = new Point(300, 10);
+                eScissors.Location = new Point(300, -130);
+            }
+            else if (player == 0 && enemy == 2)
+            {
+                winner = "Противник";
+                eRock.Location = new Point(300, -130);
+                ePaper.Location = new Point(300, -130);
+                eScissors.Location = new Point(300, 10);
+            }
+            else if (player == 1 && enemy == 0)
+            {
+                winner = "Противник";
+                eRock.Location = new Point(300, 10);
+                ePaper.Location = new Point(300, -130);
+                eScissors.Location = new Point(300, -130);
+            }
+            else if (player == 1 && enemy == 1)
+            {
+                winner = "дружба";
+                eRock.Location = new Point(300, -130);
+                ePaper.Location = new Point(300, 10);
+                eScissors.Location = new Point(300, -130);
+            }
+            else if (player == 1 && enemy == 2)
+            {
+                winner = "Игрок";
+                eRock.Location = new Point(300, -130);
+                ePaper.Location = new Point(300, -130);
+                eScissors.Location = new Point(300, 10);
+            }
+            else if (player == 2 && enemy == 0)
+            {
+                winner = "Игрок";
+                eRock.Location = new Point(300, 10);
+                ePaper.Location = new Point(300, -130);
+                eScissors.Location = new Point(300, -130);
+            }
+            else if (player == 2 && enemy == 1)
+            {
+                winner = "Противник";
+                eRock.Location = new Point(300, -130);
+                ePaper.Location = new Point(300, 10);
+                eScissors.Location = new Point(300, -130);
+            }
+            else if (player == 2 && enemy == 2)
+            {
+                winner = "дружба";
+                eRock.Location = new Point(300, -130);
+                ePaper.Location = new Point(300, -130);
+                eScissors.Location = new Point(300, 10);
+            }
+
+            
+
+            if (player != 4)
+                WhoWinner.Text = $"У противника {tEnemy} \n\n\n\n\nПобеждает {winner}!!";
+        }
+
+        public void WeaponEnemy()
+        {            
+            enemy = random.Next(3);
+
+            if (enemy == 0)
+            {
+                tEnemy = "Камень";
+            }
+            else if (enemy == 1)
+            {
+                tEnemy = "Ножницы";
+            }
+            else if (enemy == 2)
+            {
+                tEnemy = "Бумага";
+            }
+        }
+
+        public void TypeEnemy()
+        {
+
+        }
+    }
+}
